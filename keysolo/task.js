110a719b36d945695465db6e2d19d2fcae4f8d4f
Ilya Glazyrin <glazyrinilya2011@gmail.com>
2021-05-02 21:40:07 +0500

    tasks added

--- keysolo/task.js
+++ keysolo/task.js
@@ -17,13 +17,18 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    
+    document.addEventListener('keydown', succesEvent => {
+      const eventLetter = succesEvent.key.toLowerCase().charCodeAt(0);
+      const screenLetter = this.currentSymbol.textContent.toLowerCase().charCodeAt(0);
+      if (screenLetter === eventLetter) {
+          this.success();
+      } else {
+          this.fail();
+      }
+
+    });
+    
   }
 
   success() {
